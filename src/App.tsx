fc0236ca8f1f165dd5eabc286faf256496dff94d
bone-machine <12587836+bone-machine@users.noreply.github.com>
2026-07-31 17:33:22 -0300

    chore: remove tailwindcss, shadcn/ui, and related config/imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
-
-import { Button } from "@/components/ui/button"
 
 import Gallery from './Gallery';
 
@@ -12,23 +10,15 @@ const App = () =>{
   const [photosFolderName, setPhotosFolderName] = useState<string | null>(null);
   const { filteredPhotoUrls, loading } = usePhotoUrls(photosFolderName);
 
-  // Hacky, hacky
-  useEffect(() => {
-    document.documentElement.classList.add("dark");
-    return () => {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [])
-
   if (loading) return <p>Cargando fotos...</p>
 
   return (
     <div className="text-3xl font-bold underline">
       <h1>Colección</h1>
       <div>
-        <Button onClick={() => setPhotosFolderName(null)}>Todas</Button>
-        <Button onClick={() => setPhotosFolderName('analog')}>Analógicas</Button>
-        <Button onClick={() => setPhotosFolderName('digital')}>Digitales</Button>
+        <button onClick={() => setPhotosFolderName(null)}>Todas</button>
+        <button onClick={() => setPhotosFolderName('analog')}>Analógicas</button>
+        <button onClick={() => setPhotosFolderName('digital')}>Digitales</button>
       </div>
       <Gallery galleryPhotos={filteredPhotoUrls} />
     </div>
